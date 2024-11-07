c0b05251ae9c702ee66cce9dbb18b7bce83b842d
AlexPerin1983 <windowfilm.br@gmail.com>
2024-11-07 20:51:48 -0300

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,33 @@
+// utils.js
+
+// Função para mostrar toast de feedback
+function mostrarToast(titulo, mensagem, tipo = 'info') {
+    const toast = $('#feedbackToast');
+    const header = toast.find('.toast-header');
+    
+    $('#toastTitle').text(titulo);
+    $('#toastBody').text(mensagem);
+
+    // Remove todas as classes de cor anteriores
+    header.removeClass('bg-success bg-danger bg-info text-white');
+    
+    // Aplica a classe de cor apropriada
+    switch (tipo) {
+        case 'success':
+            header.addClass('bg-success text-white');
+            break;
+        case 'danger':
+            header.addClass('bg-danger text-white');
+            break;
+        default:
+            header.addClass('bg-info text-white');
+    }
+
+    toast.toast('show');
+}
+
+// Função para disparar eventos personalizados
+function dispararEvento(nome, detalhes = {}) {
+    const evento = new CustomEvent(nome, { detail: detalhes });
+    document.dispatchEvent(evento);
+}
